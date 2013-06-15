d0378fca0e0423ae84be51a32b820e85538d5b2c
Tom Wisniewski <tom.a.wisniewski@gmail.com>
2013-06-15 10:58:54 +0100

    Completed test

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,4 +2,37 @@ require 'twilio-ruby'
 
 class Takeaway
 
-end  
+  attr_accessor :total
+  
+  ACCOUNT_SID = 'ACc876bf97e06abe147e5ed97136053bbf' 
+  AUTH_TOKEN = '5fb0f7df1e68952d5a610c11f5b5078d'
+
+  def initialize
+    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+  end  
+
+  def menu
+    {  
+      fish_and_chips: 7,
+      bangers_and_mash: 7,
+      shephards_pie: 8  
+    }
+  end
+
+  def order(dishes, payment)
+    @total = dishes.inject(0) { |sum, (dish,quantity)| sum + menu[dish] * quantity }   
+    raise "You still owe me money!" unless payment == total 
+    send_text
+  end
+
+  def send_text
+    time = Time.now + 3600
+    @client.account.sms.messages.create(
+      :from => "+441183100861",
+      :to => "+447921197921",
+      :body => "Thank you! Your order was placed and will be delivered before #{time}"
+    )
+  end  
+ 
+end
+
--- tests/takeaway_test.rb
+++ tests/takeaway_test.rb
@@ -2,6 +2,39 @@ require 'test/unit'
 require 'mocha/setup'
 require '../lib/takeaway'
 
-class TestTakeaway
+class TestTakeaway < MiniTest::Unit::TestCase
+
+  def setup
+    @takeaway = Takeaway.new
+    @order = {
+      fish_and_chips: 2,
+      shephards_pie: 1,
+      bangers_and_mash: 5
+    }
+  end 
+
+  def test_cost_of_single_item_can_be_calculated
+    order1 = { fish_and_chips: 1 }
+    @takeaway.stubs(:send_text)
+    @takeaway.order(order1, 7)
+    assert_equal 7, @takeaway.total
+  end  
+
+  def test_cost_of_multiple_items_can_be_calculated
+    @takeaway.stubs(:send_text)
+    @takeaway.order(@order, 57)
+    assert_equal 57, @takeaway.total
+  end     
+
+  def test_error_is_raised_if_payment_is_not_exact
+    assert_raises(RuntimeError) do
+      assert_equal 57, @takeaway.order(@order, 56)
+    end
+  end
+
+  def test_text_is_sent_upon_successful_order
+    @takeaway.stubs(:send_text).returns("Order placed")
+    assert_equal "Order placed", @takeaway.order(@order, 57)
+  end
 
 end 
